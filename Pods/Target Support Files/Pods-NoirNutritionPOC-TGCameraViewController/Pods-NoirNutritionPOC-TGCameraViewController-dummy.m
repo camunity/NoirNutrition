0679fb3d5fcddf18027ae8d607385ff7a0eba9ca
camunity <cameron.flowers13@gmail.com>
2015-10-15 13:46:51 -0400

    pod files

---
+++ Pods/Target Support Files/Pods-NoirNutritionPOC-TGCameraViewController/Pods-NoirNutritionPOC-TGCameraViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NoirNutritionPOC_TGCameraViewController : NSObject
+@end
+@implementation PodsDummy_Pods_NoirNutritionPOC_TGCameraViewController
+@end
